78d158f7ed98d379db89b372e234c9678ef0e25f
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2022-02-12 15:10:27 +0100

    Upload file with image

--- BestGiftsAPI/Controllers/GiftIdeasController.cs
+++ BestGiftsAPI/Controllers/GiftIdeasController.cs
@@ -63,7 +63,7 @@ namespace BestGiftsAPI.Controllers
                     output.Items.Add(_mapper.Map<GiftIdeaDTO>(item));
                 }
 
-                
+
                 if (output == null)
                 {
                     return NotFound();
@@ -122,39 +122,26 @@ namespace BestGiftsAPI.Controllers
         // POST: api/GiftIdeas/PostGift
         [Route("PostGift")]
         [HttpPost]
-        public async Task<ActionResult> PostGift(IFormFile files)
+        public async Task<ActionResult> PostGift(GiftIdeaDTO giftIdeaDTO)
         {
-            if (files != null)
+            GiftIdea giftIdea = new GiftIdea();
+
+            try
             {
-                if (files.Length > 0)
-                {
-                    //Getting FileName
-                    var fileName = Path.GetFileName(files.FileName);
-                    //Getting file Extension
-                    var fileExtension = Path.GetExtension(fileName);
-                    // concatenating  FileName + FileExtension
-                    var newFileName = String.Concat(Convert.ToString(Guid.NewGuid()), fileExtension);
+                giftIdeaDTO.CreationTime = DateTime.Now;
+                var giftIdeaEntity = _mapper.Map<GiftIdea>(giftIdeaDTO);
 
-                    byte[] imageArr;
-                    using (var target = new MemoryStream())
-                    {
-                        files.CopyTo(target);
-                        imageArr = target.ToArray();
-                    }
-
-                    var giftIdeaToUpload = new GiftIdea()
-                    {
-                        ImageContent = imageArr,
-                    };
-
-
-
-                    _context.GiftIdeas.Add(giftIdeaToUpload);
-                  await _context.SaveChangesAsync();
-
-                }
+                var contextAddRes = await _context.GiftIdeas.AddAsync(giftIdeaEntity);
+                giftIdea = contextAddRes.Entity;
+                await _context.SaveChangesAsync();
             }
-            return Ok();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "[PostGift]");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+          
+            return CreatedAtAction(nameof(Get), new { id = giftIdea.GiftIdeaId }, giftIdea);
         }
     }
 }
